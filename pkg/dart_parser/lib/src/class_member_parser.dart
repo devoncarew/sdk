3e7811bd6d8574be4cef4e9b20fd205159887ae1
Peter von der Ahé <ahe@google.com>
2017-01-19 12:02:09 +0100

    Move skip methods to parser base class.
    R=sigmund@google.com
    
    Review-Url: https://codereview.chromium.org/2642663003 .

--- pkg/dart_parser/lib/src/class_member_parser.dart
+++ pkg/dart_parser/lib/src/class_member_parser.dart
@@ -5,22 +5,13 @@
 library dart_parser.class_member_parser;
 
 import 'package:dart_scanner/src/token.dart' show
-    BeginGroupToken,
-    ErrorToken,
     Token;
-
-import 'package:dart_scanner/src/token_constants.dart' show
-    EOF_TOKEN;
 
 import 'listener.dart' show
     Listener;
 
-import 'error_kind.dart' show
-    ErrorKind;
-
 import 'parser.dart' show
-    Parser,
-    optional;
+    Parser;
 
 /// Parser similar to [TopLevelParser] but also parses class members (excluding
 /// their bodies).
@@ -33,125 +24,12 @@ class ClassMemberParser extends Parser {
 
   Token parseExpression(Token token) => skipExpression(token);
 
-  Token parseArgumentsOpt(Token token) {
-    // This method is overridden for two reasons:
-    // 1. Avoid generating events for arguments.
-    // 2. Avoid calling skip expression for each argument (which doesn't work).
-    listener.handleNoArguments(token);
-    if (optional('(', token)) {
-      BeginGroupToken begin = token;
-      return begin.endGroup.next;
-    } else {
-      return token;
-    }
-  }
-
-  Token skipExpression(Token token) {
-    while (true) {
-      final kind = token.kind;
-      final value = token.stringValue;
-      if ((identical(kind, EOF_TOKEN)) ||
-          (identical(value, ';')) ||
-          (identical(value, ',')) ||
-          (identical(value, '}')) ||
-          (identical(value, ')')) ||
-          (identical(value, ']'))) {
-        break;
-      }
-      if (identical(value, '=') ||
-          identical(value, '?') ||
-          identical(value, ':') ||
-          identical(value, '??')) {
-        var nextValue = token.next.stringValue;
-        if (identical(nextValue, 'const')) {
-          token = token.next;
-          nextValue = token.next.stringValue;
-        }
-        if (identical(nextValue, '{')) {
-          // Handle cases like this:
-          // class Foo {
-          //   var map;
-          //   Foo() : map = {};
-          //   Foo.x() : map = true ? {} : {};
-          // }
-          BeginGroupToken begin = token.next;
-          token = (begin.endGroup != null) ? begin.endGroup : token;
-          token = token.next;
-          continue;
-        }
-        if (identical(nextValue, '<')) {
-          // Handle cases like this:
-          // class Foo {
-          //   var map;
-          //   Foo() : map = <String, Foo>{};
-          //   Foo.x() : map = true ? <String, Foo>{} : <String, Foo>{};
-          // }
-          BeginGroupToken begin = token.next;
-          token = (begin.endGroup != null) ? begin.endGroup : token;
-          token = token.next;
-          if (identical(token.stringValue, '{')) {
-            begin = token;
-            token = (begin.endGroup != null) ? begin.endGroup : token;
-            token = token.next;
-          }
-          continue;
-        }
-      }
-      if (!mayParseFunctionExpressions && identical(value, '{')) {
-        break;
-      }
-      if (token is BeginGroupToken) {
-        BeginGroupToken begin = token;
-        token = (begin.endGroup != null) ? begin.endGroup : token;
-      } else if (token is ErrorToken) {
-        listener.reportErrorToken(token);
-      }
-      token = token.next;
-    }
-    return token;
-  }
-
-  Token skipAsyncModifier(Token token) {
-    String value = token.stringValue;
-    if (identical(value, 'async')) {
-      token = token.next;
-      value = token.stringValue;
-
-      if (identical(value, '*')) {
-        token = token.next;
-      }
-    } else if (identical(value, 'sync')) {
-      token = token.next;
-      value = token.stringValue;
-
-      if (identical(value, '*')) {
-        token = token.next;
-      }
-    }
-    return token;
-  }
+  // This method is overridden for two reasons:
+  // 1. Avoid generating events for arguments.
+  // 2. Avoid calling skip expression for each argument (which doesn't work).
+  Token parseArgumentsOpt(Token token) => skipArgumentsOpt(token);
 
   Token parseFunctionBody(Token token, bool isExpression, bool allowAbstract) {
-    assert(!isExpression);
-    token = skipAsyncModifier(token);
-    String value = token.stringValue;
-    if (identical(value, ';')) {
-      if (!allowAbstract) {
-        listener.reportError(token, ErrorKind.EXPECTED_BODY);
-      }
-      listener.handleNoFunctionBody(token);
-    } else {
-      if (identical(value, '=>')) {
-        token = parseExpression(token.next);
-        expectSemicolon(token);
-      } else if (value == '=') {
-        token = parseRedirectingFactoryBody(token);
-        expectSemicolon(token);
-      } else {
-        token = skipBlock(token);
-      }
-      listener.skippedFunctionBody(token);
-    }
-    return token;
+    return skipFunctionBody(token, isExpression, allowAbstract);
   }
 }
--- pkg/dart_parser/lib/src/top_level_parser.dart
+++ pkg/dart_parser/lib/src/top_level_parser.dart
@@ -5,20 +5,13 @@
 library dart_parser.top_level_parser;
 
 import 'package:dart_scanner/src/token.dart' show
-    BeginGroupToken,
     Token;
-
-import 'package:dart_scanner/src/characters.dart' show
-    $CLOSE_CURLY_BRACKET;
 
 import 'listener.dart' show
     Listener;
 
 import 'class_member_parser.dart' show
     ClassMemberParser;
-
-import 'parser.dart' show
-    optional;
 
 /// Parser which only parses top-level elements, but ignores their bodies.
 /// Use [Parser] to parse everything.
@@ -29,17 +22,5 @@ class TopLevelParser extends ClassMemberParser {
       : super(listener, asyncAwaitKeywordsEnabled: asyncAwaitKeywordsEnabled,
           enableGenericMethodSyntax: enableGenericMethodSyntax);
 
-  Token parseClassBody(Token token) {
-    if (!optional('{', token)) {
-      return listener.expectedClassBodyToSkip(token);
-    }
-    BeginGroupToken beginGroupToken = token;
-    Token endGroup = beginGroupToken.endGroup;
-    if (endGroup == null) {
-      return listener.unmatched(beginGroupToken);
-    } else if (!identical(endGroup.kind, $CLOSE_CURLY_BRACKET)) {
-      return listener.unmatched(beginGroupToken);
-    }
-    return endGroup;
-  }
+  Token parseClassBody(Token token) => skipClassBody(token);
 }
